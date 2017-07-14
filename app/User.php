7015085fa152fa0369db99b3fe92f28fed264643
nadia1412 <nadiaerra06@gmail.com>
2017-07-14 08:22:43 +0800

    i make a change

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'phone_number', 'message',
+        'name', 'email', 'password', 'phone_number', 
     ];
 
     /**
